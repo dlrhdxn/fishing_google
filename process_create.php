2ad356fc93e91db73db47bfecbd62163c0d15e32
dlrhdxn <52860565+dlrhdxn@users.noreply.github.com>
2021-01-02 02:11:05 +0900

    Update process_create.php

--- process_create.php
+++ process_create.php
@@ -9,8 +9,8 @@ var_dump($_POST);
 $sql="
     INSERT INTO topic(email,pw)
     VALUES(
-        '{$_GET['email']}',
-        '{$_GET['pw']}'
+        '{$_POST['email']}',
+        '{$_POST['pw']}'
     )
 
 ";
